5607ae9f16b2bd613b7524ab562e81a881ea4c82
irenedovydenko <58356741+irenedovydenko@users.noreply.github.com>
2019-12-08 10:47:11 +0300

    Add files via upload

---
+++ A5.Practice/js/ryaba.js
@@ -0,0 +1,53 @@
+const dataURL = "https://api.myjson.com/bins/jcmhn";
+const fields = [
+	"var1",
+	"var2",
+	"var3",
+	"var4",
+	"var5",
+	"var6",
+	"speach"
+]
+
+function getFormValues() {
+	let obj = {};
+
+	fields.forEach(function(field) {
+		obj[field] = $("input[name=" + field + "]")[0].value
+	});
+
+	return obj;
+}
+
+function handleButton() {
+	$.getJSON(dataURL, handleData); 
+	$("form").hide();// взять данные по dataUrl, вытащить их и передать в handleData
+}
+
+function handleData(data) {
+  // кажется, какой-то из этих способов сработает
+  //const var1 = $("input[name=var1]")[0].value()
+  //const var1 = $("input[name=var1]").value()
+  //const var1 = $("input[name=var1]")[0].default()
+
+  // надо сделать так чтобы сообщения подменились на значения из формы
+    let message = "";
+
+    let values = getFormValues();
+
+    data["text"].forEach(function(line) {
+    	for (key in values) {
+    		line = line.replace("{" + key + "}", values[key]);
+    	}
+
+    	message = message + line + "<BR>";
+    	});
+    
+	$("div#result").html(message);
+}
+
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
